eff88e7eaed552d46914cedfac7a372ab40ad8fb
Galilea-Ivy <ggarcia87@ivytech.edu>
2022-05-02 22:15:47 -0400

    deleting and added items to user cart

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 
 
-export const products = [{
+const products = [{
     id: '123',
     name: 'Running Shoes',
     price: '60.00',
@@ -90,7 +90,7 @@ export const products = [{
     averageRating: '5.0',
   }];
   
-export const cartItems = [
+export let cartItems = [
     products[0],
     products[2],
     products[3],
@@ -117,6 +117,24 @@ app.get('/api/products/:productId', (req, res) => {
     else {
         res.status(404).json('Could not find the product');
     }
+});
+
+app.post('/api/users/:userId/cart', (req, res) => {
+    const { productId } = req.body;
+    const product = products.find(product => product.id === productId);
+    if (product) {
+        cartItems.push(product);
+        res.status(200).json(cartItems);
+    }
+    else {
+        res.status(404).json('Could not find product!');
+    }
+});
+
+app.delete('/api/users/:userId/cart/:productId', (req, res) => {
+    const { productId } = req.params;
+    cartItems = cartItems.filter(product => product.id !== productId);
+    res.status(200).json(cartItems);
 });
 
 app.listen(8000, () => {
